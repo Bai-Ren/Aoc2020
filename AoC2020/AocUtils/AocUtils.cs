1c2beb4ad009f9d7abeda59fb7f147360081b62f
Joshua Day <loves@home.jday.me>
2020-12-06 01:48:33 -0800

    day 1 to utils

---
+++ AoC2020/AocUtils/AocUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AocUtils
+{
+    public class AocUtils
+    {
+        public static void ReadInput(Action<string> action)
+        {
+            IEnumerable<string> lines = File.ReadAllLines("input.txt");
+
+            foreach (string line in lines)
+            {
+                action.Invoke(line);
+            }
+        }
+    }
+}
--- AoC2020/ReportRepair/Program.cs
+++ AoC2020/ReportRepair/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AocUtils;
 
 namespace ReportRepair
 {
     class Program
     {
+        static HashSet<int> Nums = new HashSet<int>();
+        static Dictionary<int, int> SumsToProducts = new Dictionary<int, int>();
         static void Main(string[] args)
         {
             PartOne();
@@ -22,63 +22,63 @@ namespace ReportRepair
             Console.WriteLine("PartOne");
             Console.WriteLine("-------");
 
-            IEnumerable<string> lines = File.ReadAllLines("input.txt");
+            Nums = new HashSet<int>();
 
-            HashSet<int> nums = new HashSet<int>();
+            AocUtils.AocUtils.ReadInput(PartOneAction);
+        }
 
-            foreach (string line in lines)
+        static void PartOneAction (string line)
+        {
+            try
             {
-                try
-                {
-                    int num = int.Parse(line);
+                int num = int.Parse(line);
 
-                    nums.Add(num);
-                    if (nums.Contains(2020 - num))
-                    {
-                        Console.WriteLine("Found pair, {0} {1}", num, 2020 - num);
-                        Console.WriteLine("Product: {0}", num * (2020 - num));
-                    }
-                }
-                catch (Exception ex)
+                Nums.Add(num);
+                if (Nums.Contains(2020 - num))
                 {
-                    Console.WriteLine("Failed to process line:{0} | error:{1}", line, ex);
+                    Console.WriteLine("Found pair, {0} {1}", num, 2020 - num);
+                    Console.WriteLine("Product: {0}", num * (2020 - num));
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to process line:{0} | error:{1}", line, ex);
             }
         }
         static void PartTwo()
         {
+            Console.WriteLine("");
             Console.WriteLine("PartTwo");
             Console.WriteLine("-------");
 
-            IEnumerable<string> lines = File.ReadAllLines("input.txt");
+            Nums = new HashSet<int>();
+            SumsToProducts = new Dictionary<int, int>();
 
-            HashSet<int> nums = new HashSet<int>();
-            Dictionary<int, int> sumsToProducts = new Dictionary<int, int>();
+            AocUtils.AocUtils.ReadInput(PartTwoAction);
+        }
 
-            foreach (string line in lines)
+        static void PartTwoAction (string line)
+        {
+            try
             {
-                try
+                int num = int.Parse(line);
+
+                if (SumsToProducts.ContainsKey(2020 - num))
                 {
-                    int num = int.Parse(line);
-
-                    if (sumsToProducts.ContainsKey(2020 - num))
-                    {
-                        Console.WriteLine("Found trio");
-                        Console.WriteLine("Product: {0}", sumsToProducts[2020 - num] * num);
-                        break;
-                    }
-
-                    foreach(int num2 in nums)
-                    {
-                        sumsToProducts[num + num2] = num * num2;
-                    }
-
-                    nums.Add(num);
+                    Console.WriteLine("Found trio");
+                    Console.WriteLine("Product: {0}", SumsToProducts[2020 - num] * num);
                 }
-                catch (Exception ex)
+
+                foreach (int num2 in Nums)
                 {
-                    Console.WriteLine("Failed to process line:{0} | error:{1}", line, ex);
+                    SumsToProducts[num + num2] = num * num2;
                 }
+
+                Nums.Add(num);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to process line:{0} | error:{1}", line, ex);
             }
         }
     }
